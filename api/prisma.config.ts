8ec626747ab3a181f08bb7e1bd1a9befaad860ce
Your Name <you@example.com>
2025-10-19 20:26:41 +0100

    Add prisma.config.ts to fix schema path

--- api/prisma.config.ts
+++ api/prisma.config.ts
@@ -1,3 +1,6 @@
-export default {
-  schema: 'prisma/schema.prisma',
-}
+// api/prisma.config.ts
+import { defineConfig } from "@prisma/config";
+
+export default defineConfig({
+  schema: "./prisma/schema.prisma", // <- points to api/prisma/schema.prisma
+});
